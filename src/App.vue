c5ffd9cc3a9dbcdf3158b3c901c7c9fc756204cc
Divyansh Tripathi <3151-divyansh.t.aux@users.noreply.source.golabs.io>
2020-08-20 19:16:48 +0530

    route highlighting

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,38 @@
 <template>
   <div id="app">
-		<div class="side-bar">
+    <div class="side-bar">
       <h5 class="my-4">Interview Creation Portal</h5>
       <ul class="side-list">
-        <router-link tag="li" class="side-item" to="/">Create Interview</router-link>
-        <router-link tag="li" class="side-item" to="/interviews">Upcoming Interviews</router-link>
+        <router-link
+          tag="li"
+          :class="selectedRoute == '/' ? 'selected' : ''"
+          class="side-item"
+          to="/"
+          >Create Interview</router-link
+        >
+        <router-link
+          tag="li"
+          class="side-item"
+          :class="selectedRoute == '/interviews' ? 'selected' : ''"
+          to="/interviews"
+          >Upcoming Interviews</router-link
+        >
       </ul>
     </div>
     <div class="main-content">
-    	<router-view />
+      <router-view />
     </div>
   </div>
 </template>
 
 <script>
 export default {
-	computed: {
-		selectedRoute() {
-			return this.$route.name;
-		}
-	},
-}
+  computed: {
+    selectedRoute() {
+      return this.$route.path;
+    },
+  },
+};
 </script>
 
 <style lang="scss">
@@ -29,44 +41,42 @@ body {
 }
 #app {
   height: 100vh;
-	display: flex;
+  display: flex;
 }
 
 .side-bar {
-	flex: 1;
-	height: 100%;
-	background: rgb(219, 99, 0);
-	color: white;
-	display: flex;
-	flex-direction: column;
-	padding: 20px 0;
-	text-align: center;
-	box-shadow: 5px 0 20px rgba(0, 0, 0, 0.3);
-	z-index: 10;
+  flex: 1;
+  height: 100%;
+  background: rgb(219, 99, 0);
+  color: white;
+  display: flex;
+  flex-direction: column;
+  padding: 20px 0;
+  text-align: center;
+  box-shadow: 5px 0 20px rgba(0, 0, 0, 0.3);
+  z-index: 10;
 
-	.side-list {
-		list-style: none;
-		padding: 0;
-		margin-top: 30px;
+  .side-list {
+    list-style: none;
+    padding: 0;
+    margin-top: 30px;
 
-		.side-item {
-			padding: 20px 0;
-			background: rgba(255, 255, 255, 0.1);
-			cursor: pointer;
-
-			&.selected {
-				background: rgb(46, 46, 255);
-			}
-		}
-	}
+    .side-item {
+      padding: 20px 0;
+      cursor: pointer;
+      &.selected {
+        background: rgba(255, 255, 255, 0.1);
+      }
+    }
+  }
 }
 .main-content {
-	height: 100%;
-	overflow: auto;
-	background: #f9f9f9;
-	color: black;
-	flex: 4;
-	width: 100%;
-	padding: 20px;
+  height: 100%;
+  overflow: auto;
+  background: #f9f9f9;
+  color: black;
+  flex: 4;
+  width: 100%;
+  padding: 20px;
 }
 </style>
